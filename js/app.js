a2e40938f8f2fc72db9ec276774dfbe585d1adb5
alemalagni <alemalagni@gmail.com>
2025-03-05 16:43:27 +0100

    feat: add JS script

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 // Dichiarazione variabili
-const km = document.getElementById("km");
-const età = document.getElementById("età");
 const genera = document.getElementById("genera");
 let totale = 0;
 
 // Script
 genera.addEventListener('click',
     function () {
-        totale = km.innerHTML * 0.21;
+        const km = document.getElementById("km");
+        const età = document.getElementById("età");
 
-        if ( età.innerHTML < 18 ) {
+        totale = km.value * 0.21;
+
+        if ( età.value < 18 ) {
             totale = totale * 0.80;
-        } else if ( età.innerHTML >= 65 ) {
+        } else if ( età.value >= 65 ) {
             totale = totale * 0.60;
         }
 
